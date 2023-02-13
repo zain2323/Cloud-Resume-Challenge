ab488733c4e96f5c8faa2299b46aa601a247f5b0
Zain Siddiqui <zainsiddiqui2323@gmail.com>
2023-02-13 16:15:58 +0500

    added achievements on the sidebar section

--- src/components/SiderBar.jsx
+++ src/components/SiderBar.jsx
@@ -15,6 +15,7 @@ import GitHubIcon from "@mui/icons-material/GitHub";
 import medium from "../assets/medium.svg";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
+import CircleIcon from "@mui/icons-material/Circle";
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -109,12 +110,19 @@ export default function SideBar() {
         </Stack>
       </Stack>
       <Stack sx={{ mt: 2, mb: 2 }} spacing={0.5}>
+        <Typography variant="h4">Achievements</Typography>
+        <Typography>
+          Published multiple articles on Medium, covering topics such as
+          tutorials on Linux and software development.
+        </Typography>
+      </Stack>
+      <Stack sx={{ mt: 2, mb: 2 }} spacing={0.5}>
         <Typography variant="h4">Reach me </Typography>
         <Link color="inherit" underline="hover" variant="body1">
-            <Stack direction="row" spacing={0.7}>
+          <Stack direction="row" spacing={0.7}>
             <AlternateEmailIcon />
-          <Typography variant="body1">zainsiddiqui2323@gmail.com</Typography>
-            </Stack>
+            <Typography variant="body1">zainsiddiqui2323@gmail.com</Typography>
+          </Stack>
         </Link>
         <Stack direction="row" spacing={2}>
           <Link href="https://github.com/zain2323" color="inherit">
