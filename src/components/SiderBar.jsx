c2957f5e57eae07079d3f60f73c0062d64c3445e
Zain Siddiqui <zainsiddiqui2323@gmail.com>
2023-02-13 09:24:13 +0500

    complete sidebar section

--- src/components/SiderBar.jsx
+++ src/components/SiderBar.jsx
@@ -7,8 +7,14 @@ import {
   Paper,
   styled,
   Box,
+  Link,
 } from "@mui/material";
 import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import medium from "../assets/medium.svg";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -16,6 +22,10 @@ const Item = styled(Paper)(({ theme }) => ({
   padding: theme.spacing(1),
   textAlign: "center",
   color: theme.palette.text.secondary,
+  "&:hover": {
+    background: "gray",
+    color: "black",
+  },
 }));
 
 export default function SideBar() {
@@ -53,8 +63,8 @@ export default function SideBar() {
           </Typography>
         </Stack>
       </Stack>
-      <Divider i />
-      <Box sx={{ mt: 2, mr: 2 }}>
+      <Divider />
+      <Box sx={{ mt: 2, mr: 2, mb: 2 }}>
         <Typography variant="h5">Technical Skills</Typography>
         <Grid
           container
@@ -71,6 +81,57 @@ export default function SideBar() {
           })}
         </Grid>
       </Box>
+      <Divider />
+      <Stack sx={{ mt: 2, mb: 2 }} spacing={0.5}>
+        <Typography variant="h5">Certifications</Typography>
+        <Typography variant="body1">
+          AWS Certified Cloud Practitioner
+        </Typography>
+        <Stack direction="row" spacing={0.5}>
+          <CalendarTodayIcon />
+          <Typography variant="body1" color="gray">
+            May 2023 - May 2025
+          </Typography>
+        </Stack>
+        <Typography variant="body1">Advanced React Bootcamp</Typography>
+        <Stack direction="row" spacing={0.5}>
+          <LocationOnIcon />
+          <Typography variant="body1" color="gray">
+            Scrimba
+          </Typography>
+        </Stack>
+        <Typography variant="body1">SQL Nanodegree</Typography>
+        <Stack direction="row" spacing={0.5}>
+          <LocationOnIcon />
+          <Typography variant="body1" color="gray">
+            Udacity
+          </Typography>
+        </Stack>
+      </Stack>
+      <Stack sx={{ mt: 2, mb: 2 }} spacing={0.5}>
+        <Typography variant="h5">Reach me </Typography>
+        <Link color="inherit" underline="hover" variant="body1">
+          <Stack direction="row" spacing={0.7}>
+            <AlternateEmailIcon />
+            <Typography variant="body1">zainsiddiqui2323@gmail.com</Typography>
+          </Stack>
+        </Link>
+        <Stack direction="row" spacing={2}>
+          <Link href="https://github.com/zain2323" color="inherit">
+            <GitHubIcon fontSize="large" />
+          </Link>
+          <Link href="https://medium.com/@zainsiddiqui2323" color="inherit">
+            <img
+              src={medium}
+              alt="Medium svg icon"
+              style={{ width: 50, height: 40 }}
+            />
+          </Link>
+          <Link href="https://twitter.com/ZainSid11526801" color="inherit">
+            <TwitterIcon fontSize="large" />
+          </Link>
+        </Stack>
+      </Stack>
     </>
   );
 }
